785b396078f2e8629d28d65d3856821a6df8cf2f
asma <mlle.am01@gmail.com>
2023-05-03 19:30:45 -0700

    updates

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,31 +1,34 @@
 #include "lists.h"
 
 /**
- * 
+ * find_listint_loop - it finds  loop in linked list
+ * @head: linked list to search for..
+ * Return: where the loop starts, or NULL
  */
-listint_t *find_listint_loop(listint_t *head)
+listint_t *find_listint_loop(listint_t *list_head)
 {
-	listint_t *tortoise = head;
-	listint_t *hare = head;
+    listint_t *slow_ptr = list_head;
+    listint_t *fast_ptr = list_head;
 
-	if (!head)
-		return (NULL);
+    if (!list_head)
+        return (NULL);
 
-	while (tortoise && hare && hare->next)
-	{
-		hare = hare->next->next;
-		tortoise = tortoise->next;
-		if (hare == tortoise)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			return (hare);
-		}
-	}
+    while (slow_ptr && fast_ptr && fast_ptr->next)
+    {
+        fast_ptr = fast_ptr->next->next;
+        slow_ptr = slow_ptr->next;
+        if (fast_ptr == slow_ptr)
+        {
+            slow_ptr = list_head;
+            while (slow_ptr != fast_ptr)
+            {
+                slow_ptr = slow_ptr->next;
+                fast_ptr = fast_ptr->next;
+            }
+            return (fast_ptr);
+        }
+    }
 
-	return (NULL);
+    return (NULL);
 }
+
